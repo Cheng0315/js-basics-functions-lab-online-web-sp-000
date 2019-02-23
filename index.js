400d1a03a812a6fa218eb41c0ce2816b0df230ea
Cheng Vue <csalvue@gmail.com>
2019-02-23 12:39:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  return Math.abs(distance - 42);
+}
+
+function distanceFromHqInFeet(distance) {
+  return distanceFromHqInBlocks(distance) + 263;
+}
