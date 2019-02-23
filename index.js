0f2050d111c80117d120c41541e8b25ecb9b85e7
Cheng Vue <csalvue@gmail.com>
2019-02-23 12:53:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function distanceFromHqInFeet(distance) {
 function distanceTravelledInFeet(destination1, destination2) {
   return Math.abs((destination1 - destination2) * 264)
 }
+
+
+function calculatesFarePrice(start, destination) {
+  distance = distanceTravelledInFeet(start, destination)
+
+  if (distance <= 400) {
+    return 0
+  } else if (distance > 400 && distance <= 2000) {
+    return distance * 0.02
+  }
+}
