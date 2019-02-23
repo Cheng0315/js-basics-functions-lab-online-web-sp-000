3e6ae79522eb1e4141d8380720545a66d791c93f
Cheng Vue <csalvue@gmail.com>
2019-02-23 12:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,9 @@ function calculatesFarePrice(start, destination) {
     return 0
   } else if (distance > 400 && distance <= 2000) {
     return (distance - 400) * 0.02
+  } else if () {
+    
+  } else {
+    return 'cannot travel that far'
   }
 }
