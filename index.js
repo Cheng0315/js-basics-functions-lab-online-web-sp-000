c792c695512ef45fb7202584027fbbc7527c5f15
Cheng Vue <csalvue@gmail.com>
2019-02-23 12:42:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function distanceFromHqInBlocks(distance) {
 function distanceFromHqInFeet(distance) {
   return distanceFromHqInBlocks(distance) * 264;
 }
+
+function distanceTravelledInFeet(distance) {
+
+}
